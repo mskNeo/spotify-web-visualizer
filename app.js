dc40edd91ee4c6baf9e2a0c2a05fab9752c304cd
Mahdeen Khan <mahdeen.s.khan@yale.edu>
2022-04-02 17:07:49 -0400

    heroku deployment

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const creds = {
     redirectUri: process.env.REDIRECT_URI,
 };
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.port || 3001;
 
 const app = express();
 app.use(cors())
