3ce0ea4a37cf2cfb5f89e51470cb7cd8279e0383
Mahdeen Khan <mahdeen.s.khan@yale.edu>
2022-04-02 18:47:09 -0400

    fixing redirect

--- client/src/App.js
+++ client/src/App.js
@@ -3,16 +3,14 @@ import Login from './components/Login';
 import Dashboard from './components/Dashboard';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
+export default function App() {
   const [code, setCode] = useState();
 
   useEffect(() => {
     setCode(() => new URLSearchParams(window.location.search).get('code'));
-  }, [])
+  }, []);
 
   return (
     code ? <Dashboard code={code} /> : <Login />
   );
 }
-
-export default App;
