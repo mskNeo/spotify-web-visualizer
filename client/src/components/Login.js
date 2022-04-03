09da5127d4d0de7cf712b66496cc811973a59bdd
Mahdeen Khan <mahdeen.s.khan@yale.edu>
2022-04-03 14:31:11 -0400

    heroku deployment

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
 
-const { REACT_APP_CLIENT_ID, REACT_APP_REDIRECT_URI } = process.env;
+// const { REACT_APP_CLIENT_ID, REACT_APP_REDIRECT_URI } = process.env;
 
 const scope = [
     'user-read-email',
@@ -10,7 +10,8 @@ const scope = [
     'user-read-playback-state'
 ];
 
-let AUTH_URL = `https://accounts.spotify.com/authorize?client_id=${REACT_APP_CLIENT_ID}&response_type=code&redirect_uri=${REACT_APP_REDIRECT_URI}&show_dialog=true&scope=streaming`;
+let AUTH_URL = 'https://accounts.spotify.com/authorize?client_id=5df20bbf39b1459d83e0182097b39224&response_type=code&redirect_uri=https://spotify-web-visualizer.herokuapp.com/&show_dialog=true&scope=streaming
+;
 
 export default function Login() {
 
